f56a48eac3366e2eb3bd5304e9a070a102171ba6
ANTHONYPC\Limn01 <anthonylimnios@hotmail.com>
2017-10-04 15:41:09 +1100

    fixed the portal to go to coming soon scene

---
+++ Assets/EndOfDemoCutScene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfDemoCutScene : MonoBehaviour
+{
+    public float moveSpeed;
+    //public float waitTime;
+    public Transform currentPoint;
+    public Transform[] points;
+    public int pointIndex;
+
+    private void Start()
+    {
+        currentPoint = points[pointIndex];
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, moveSpeed * Time.deltaTime);
+
+        if (transform.position == currentPoint.position)
+        {
+            pointIndex++;
+
+            if (pointIndex == points.Length)
+            {
+                pointIndex = 0;
+            }
+
+            currentPoint = points[pointIndex];
+        }
+    }
+
+    private void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, moveSpeed * Time.deltaTime);
+
+        if (transform.position == currentPoint.position)
+        {
+            pointIndex++;
+
+            if (pointIndex == points.Length)
+            {
+                pointIndex = 0;
+            }
+
+            currentPoint = points[pointIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/OtherScripts/PortalEnter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Com.LuisPedroFonseca.ProCamera2D;
+
+public class PortalEnter : MonoBehaviour
+{
+    [SerializeField]
+    Text inputText;
+    [SerializeField]
+    float wait;
+
+    ProCamera2DTransitionsFX transition;
+
+    private void OnEnable()
+    {
+        inputText = GameObject.FindGameObjectWithTag("InputText").GetComponent<Text>();
+        transition = GameObject.FindObjectOfType<ProCamera2DTransitionsFX>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            inputText.text = ("Y");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            if (Input.GetButtonDown("YButton"))
+            {
+                StartCoroutine(Transition());
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            inputText.text = (" ");
+        }
+    }
+
+    IEnumerator Transition()
+    {
+        transition.TransitionExit();
+
+        yield return new WaitForSeconds(wait);
+
+        SceneManager.LoadScene("EndOfDemo");
+    }
+}
